444b9638e0b207f51ce32f2dda723dcc565c5fb4
Nihal <nihal9ns@gmail.com>
2019-09-26 11:22:38 +0530

    commit 3

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ console.log("HELLO WORLD!");
 console.log("HELLO WORLD!");
 console.log("commit 1 from test-branch-2");
 console.log("commit 2 from test-branch-2");
+console.log("commit 3 from test-branch-2");
